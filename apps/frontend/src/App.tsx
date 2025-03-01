432d4fcbdcb5c6d7e734e091d74fd5f667534d36
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-03-01 01:52:43 -0600

    feat: integrate routing with Layout and CreateSecret page in App component

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,17 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { Layout } from './layout/Layout'
+import { CreateSecret } from './pages/CreateSecret'
+import { Toast } from './components/Toast'
+
 export const App: React.FC = (): React.ReactNode => {
 	return (
-		<h1>ola</h1>
+		<Router>
+			<Layout>
+				<Routes>
+					<Route path='/' element={<CreateSecret />} />
+				</Routes>
+			</Layout>
+			<Toast />
+		</Router>
 	)
 }
