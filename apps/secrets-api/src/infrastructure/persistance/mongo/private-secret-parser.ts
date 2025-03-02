ad5be81c7e871fb6cd57dd6d948a7fc4a1173d86
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-03-02 03:16:28 -0600

    feat: add PrivateSecretParser for converting between domain and document representations

---
+++ apps/secrets-api/src/infrastructure/persistance/mongo/private-secret-parser.ts
@@ -0,0 +1,51 @@
+import { PrivateSecret } from '@/domain/private_secret/private-secret'
+
+export class PrivateSecretParser {
+	private _uuid: Dependencies['muid']
+
+	constructor({ muid }: Pick<Dependencies, 'muid'>) {
+		this._uuid = muid
+	}
+
+	toDomain({
+		_id,
+		created_at,
+		expires_at,
+		secret_id,
+		encrypted_secret,
+		attempts,
+		max_attempts,
+	}: PrivateSecretDocument) {
+		const id = _id.toString()
+		return new PrivateSecret({
+			id,
+			createdAt: created_at,
+			expiresAt: expires_at,
+			secretId: secret_id,
+			encryptedSecret: encrypted_secret,
+			attempts,
+			maxAttempts: max_attempts,
+		})
+	}
+
+	toDocument({
+		id,
+		createdAt,
+		expiresAt,
+		secretId,
+		encryptedSecret,
+		attempts,
+		maxAttempts,
+	}: PrivateSecretDomain) {
+		const _id = this._uuid.from(id)
+		return {
+			_id,
+			created_at: createdAt,
+			expires_at: expiresAt,
+			secret_id: secretId,
+			encrypted_secret: encryptedSecret,
+			attempts: attempts,
+			max_attempts: maxAttempts,
+		}
+	}
+}
