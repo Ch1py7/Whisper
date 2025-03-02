9f2ccde79560dd3929c1fc02860d64f8a50b1405
ch1py7 <garcialopezgerardo6612@gmail.com>
2025-03-01 18:57:49 -0600

    feat: register SavePublicSecret and crypto in the dependency container

--- apps/secrets-api/src/container.ts
+++ apps/secrets-api/src/container.ts
@@ -1,8 +1,10 @@
 import { InjectionMode, asClass, asFunction, asValue, createContainer } from 'awilix'
+import crypto from 'node:crypto'
 import muid from 'uuid-mongodb'
+import { SavePublicSecret } from './application/save_public_secret'
 import { DbHandler } from './infrastructure/persistance/mongo/db-handler'
-import { PublicSecretParser } from './infrastructure/persistance/mongo/public-secret-parser'
 import { PublicSecretRepository } from './infrastructure/persistance/mongo/mongo-public-secret-repository'
+import { PublicSecretParser } from './infrastructure/persistance/mongo/public-secret-parser'
 
 const container = createContainer<Dependencies>({
 	injectionMode: InjectionMode.PROXY,
@@ -11,8 +13,10 @@ const container = createContainer<Dependencies>({
 container.register({
 	dbHandler: asFunction(DbHandler),
 	muid: asValue(muid),
+	crypto: asValue(crypto),
 	publicParser: asClass(PublicSecretParser),
-	publicSecretRepository: asClass(PublicSecretRepository)
+	publicSecretRepository: asClass(PublicSecretRepository),
+	savePublicSecret: asClass(SavePublicSecret),
 })
 
 export default container
--- apps/secrets-api/src/types/container.d.ts
+++ apps/secrets-api/src/types/container.d.ts
@@ -1,13 +1,17 @@
+import type { SavePublicSecret } from '@/application/save_public_secret'
 import type { DbHandler } from '@/infrastructure/persistance/mongo/db-handler'
 import type { PublicSecretRepository } from '@/infrastructure/persistance/mongo/mongo-public-secret-repository'
 import type { PublicSecretParser } from '@/infrastructure/persistance/mongo/public-secret-parser'
+import type crypto from 'node:crypto'
 import type muid from 'uuid-mongodb'
 
 declare global {
 	interface Dependencies {
 		dbHandler: ReturnType<typeof DbHandler>
 		muid: typeof muid
+		crypto: typeof crypto
 		publicParser: PublicSecretParser
 		publicSecretRepository: PublicSecretRepository
+		savePublicSecret: SavePublicSecret
 	}
 }
